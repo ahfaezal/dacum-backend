b68fc359fd2c368a27d8fb4e41242773d3b78a38
ahfaezal <ahfaezal@gmail.com>
2026-01-13 14:23:52 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+const sessions = {};
+
+app.post("/submit", (req, res) => {
+  const { session, name, activity } = req.body;
+  if (!sessions[session]) sessions[session] = [];
+
+  const card = {
+    id: Date.now(),
+    name,
+    activity,
+    time: new Date().toISOString()
+  };
+
+  sessions[session].push(card);
+  io.to(session).emit("new-card", card);
+
+  res.json({ success: true });
+});
+
+app.get("/cards/:session", (req, res) => {
+  res.json(sessions[req.params.session] || []);
+});
+
+io.on("connection", (socket) => {
+  socket.on("join", (session) => {
+    socket.join(session);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("DACUM Backend running");
+});
